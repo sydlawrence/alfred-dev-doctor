8cea9ad6ecdcf45357721a448dd061fd3ab1582d
Syd Lawrence <sydlawrence@gmail.com>
2013-03-24 20:18:17 +0000

    fixed url generation

--- parsers/autosuggest/classes/python/PythonParser.php
+++ parsers/autosuggest/classes/python/PythonParser.php
@@ -8,6 +8,23 @@ Class PythonParser extends AutoSuggestParser {
 
     public $icon = "icon.png";
 
+    public function filter_url($url) {
+        $params = explode("#", $url);
+        if (count($params) < 2) return $url;
+        $end = explode(".",$params[1]);
+
+        $meh = explode("_",$end[0]);
+        if (count($meh) == 2) {
+            if ($meh[0] == $meh[1]) {
+                unset($meh[1]);
+            }
+        }
+        $end[0] = implode("_", $meh);
+        $params[1] = implode(".",$end);
+        $url = implode("#",$params);
+        return str_replace("#_","#",$url);
+    }
+
     protected function addResults($arr) {
         foreach ($arr as $key => $val) {
             $title = $val->title;
@@ -16,7 +33,9 @@ Class PythonParser extends AutoSuggestParser {
             $this->addResult($url, $title, $description);
 
             foreach ($val->searchableItems as $res) {
-                $this->addResult($url."#".$res->domId, $res->name, "");
+
+                $url2 = $this->filter_url($url."#".$res->domId);
+                $this->addResult($url2, $res->name, "");
             }
         }
     }
--- test.php
+++ test.php
@@ -1,10 +1,24 @@
 <?php
 
-$type = "css";
+$type = "python";
 $query = "trans";
 
-
-
 require_once('search.php');
+
+
+
+
+$url = "http://docs.python.org/library/struct.html#struct_struct.pack";
+
+echo $url;
+
+echo $parser->filter_url($url);
+
+
+
+exit;
+
+
+
 
 $parser->update();
