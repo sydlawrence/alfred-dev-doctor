f7e0e2f544e1a04c01a3efaf63c0ca2196704c15
Mike Jablecki <mjablecki@gmail.com>
2013-04-17 17:55:37 -0300

    Fixes extra text in alfred popup, removes namespace only links

--- parsers/autosuggest/classes/sf2/Sf2Parser.php
+++ parsers/autosuggest/classes/sf2/Sf2Parser.php
@@ -11,13 +11,18 @@ Class Sf2Parser extends AutoSuggestParser {
     private $docVer = "2.2";
 
     protected function addResults($arr) {
+
         foreach ($arr as $key => $val) {
+
             $baseUrl = 'api.symfony.com';
 
+            // Seems to remove namespace only entries
+            if (empty($val[4])) continue;
+
             $this->results[] = array(
-                "url" => sprintf("http://%s/%s/%s", $baseUrl, $this->docVer, $val[2]),
-                "title" => $val[0],
-                "description" => sprintf("%s\n%s\n%s", $val[1], $val[3], $val[4])
+                "url"         => sprintf("http://%s/%s/%s", $baseUrl, $this->docVer, $val[2]),
+                "title"       => sprintf("%s %s", $val[0], strip_tags(preg_replace('#<a.*?>.*?</a>#i', '',$val[3]))),
+                "description" => sprintf("%s %s", $val[1], $val[4])
             );
         }
     }
